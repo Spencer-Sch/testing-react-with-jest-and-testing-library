cf9ed589bef510bff039073f5de6a4a8669e8e56
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-04-20 05:47:59 -0500

    Section 4: Sundaes On Demand: Form Review and Popover - Complete!

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.js
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.js
@@ -1,4 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import SummaryForm from '../SummaryForm';
@@ -35,13 +39,28 @@ describe('Checkbox and button tests', () => {
 });
 
 describe('Popover tests', () => {
-  test('popover responds to hover', () => {
+  test('popover responds to hover', async () => {
     render(<SummaryForm />);
 
     // popover starts out hidden
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
 
     // popover appears upon mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    userEvent.hover(termsAndConditions);
+
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
 
     // popover disapears when we mouse out
+    userEvent.unhover(termsAndConditions);
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText(/no ice cream will actually be delivered/i)
+    );
   });
 });
